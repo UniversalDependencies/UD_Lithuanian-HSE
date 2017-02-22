1a4f1e33b793c5117598b6f8ff51d3fbccd004e6
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-22 18:27:47 +0100

    make sure punct nodes have no children

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -7,6 +7,7 @@ L=lt
 for a in train dev test; do
     cat $L-ud-$a.conllu | sed 's/newpar_id/newpar id/' | udapy -s \
       util.Eval node='node.misc = "En=" + str(node.misc)[:-2].replace(" ", "_")' \
+      ud.FixPunctChild \
       ud.SetSpaceAfterFromText \
     > ../$L-ud-$a.conllu
 done
