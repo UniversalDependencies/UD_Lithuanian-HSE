7264307980e3af63512e0df90833ca2f479c2d0e
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-22 22:28:21 +0100

    flat->appos where appropriate
    All cases manually checked.

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -10,6 +10,7 @@ for a in train dev test; do
       util.Eval node='if node.lemma[0].isupper() and (node.form[0].islower() or (node.ord==1 and node.upos!="PROPN" and node.misc["En"][0].islower())): node.lemma=node.lemma.lower()' \
       util.Eval node='if node.lemma[0].isupper() and node.upos=="NOUN": node.upos="PROPN"' \
       util.Eval node='if node.feats["Degree"]=="Pos" and node.upos not in ("ADJ", "ADV"): del node.feats["Degree"]' \
+      util.Eval node='if node.deprel=="flat" and node.parent.upos=="NOUN": node.deprel="appos"' \
       ud.FixPunctChild \
       ud.SetSpaceAfterFromText \
     > ../$L-ud-$a.conllu
