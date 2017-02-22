2e86aa2ce9b2286bfc776410cb4055070caa2a7d
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-22 18:46:28 +0100

    delete Degree=Pos if upos!=ADJ|ADV

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -7,6 +7,7 @@ L=lt
 for a in train dev test; do
     cat $L-ud-$a.conllu | sed 's/newpar_id/newpar id/' | udapy -s \
       util.Eval node='node.misc = "En=" + str(node.misc)[:-2].replace(" ", "_")' \
+      util.Eval node='if node.feats["Degree"]=="Pos" and node.upos not in ("ADJ", "ADV"): del node.feats["Degree"]' \
       ud.FixPunctChild \
       ud.SetSpaceAfterFromText \
     > ../$L-ud-$a.conllu
